0c5bae924bcf6049751cb567862415273af65d85
moneromooo-monero <moneromooo-monero@users.noreply.github.com>
2020-05-17 17:12:11 +0000

    cryptonote_protocol: sanity check object requests to mitigate dos potential

--- tests/core_proxy/core_proxy.h
+++ tests/core_proxy/core_proxy.h
@@ -111,5 +111,6 @@ namespace tests
     bool prune_blockchain(uint32_t pruning_seed) const { return true; }
     bool get_txpool_complement(const std::vector<crypto::hash> &hashes, std::vector<cryptonote::blobdata> &txes) { return false; }
     bool get_pool_transaction_hashes(std::vector<crypto::hash>& txs, bool include_unrelayed_txes = true) const { return false; }
+    bool is_request_sane(const cryptonote::NOTIFY_REQUEST_GET_OBJECTS::request& arg, std::string &error_message) const { return true; }
   };
 }
